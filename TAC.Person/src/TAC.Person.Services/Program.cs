5b3675b43f895d87898a528552478f9dc3cc80c1
Eduardo Ferreira <eduferreira1989@gmail.com>
2023-08-25 14:32:17 +0100

    [TAC.Person][eduferreira1989] Configured dependency injection of services

--- TAC.Person/src/TAC.Person.Services/Program.cs
+++ TAC.Person/src/TAC.Person.Services/Program.cs
@@ -1,6 +1,18 @@
+using TAC.Common.MongoDB;
+using TAC.Person.Domains;
+using TAC.Person.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMongo()
+                .AddMongoRepository<TAC.Person.Models.Person>("person");
+
+builder.Services.AddScoped<IPersonService, PersonService>();
+builder.Services.AddScoped<IJobService, JobService>();
+builder.Services.AddScoped<ISalaryService, SalaryService>();
+builder.Services.AddScoped<ITaxService, TaxService>();
+builder.Services.AddScoped<IContributionService, ContributionService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
